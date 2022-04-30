a374eedca7072634ed10eb9666684d905c0d6069
martinseeger2002 <57598081+martinseeger2002@users.noreply.github.com>
2022-04-30 09:59:47 -0500

    Add files via upload

--- Omni_data_en_de_suite/Omni_data_en_de_suite.py
+++ Omni_data_en_de_suite/Omni_data_en_de_suite.py
@@ -1,7 +1,12 @@
 
+
+
+
+print('Welcome to the Omni File Suite.\n\nIf you have any questions or suggestions\nfeel free to reach out to me on twitter @martinseeger2.\
+\nIf you find this program intresting or useful\nsend ltc to MD3dviJcZjZ1gFJFdSqwtnq6nWrdNyoB4r\nhelp support the creator.\n')
 user_input = input\
-('Input the action you would like to exacute.\n\nRead commands.\nvp = view props.\nip = Image parse.\n\nWrite commands.\nga = Gennerate omni arguments.\
-\nfc = fee calulator.\nsif = Sendissuancefixed creats tokens. This will cost fees.\nsifs = Sendissuancefixed creats singal token. Wait 12 min before you do this. This will cost fees.\n:')
+('Input the action you would like to execute.\n\nRead commands:\nvp = view props.\nip = Image parse.\nmp3 = mp3 parse\n\nWrite commands:\niga = Generate omni arguments from in.jpeg image file.\nmp3ga = Generate omni arguments from in.mp3 mp3 file.\
+\nfc = fee calulator.\nsif = Sendissuancefixed creats tokens from the arguments.txt. This will cost fees.\nsifs = Creats singal token if one is missing. Wait 13 min before you do this the transaction may still be verified.\n\nInput command:')
 
 
 def fee_calc():
@@ -14,10 +19,20 @@ def image_parse():
 if user_input == 'ip':
     image_parse()
 
-def gen_argumnets():
-    import file_to_omni_argumnets_txt.file_to_omni_argumnets_txt
-if user_input == 'ga':
-    gen_argumnets()
+def mp3_parse():
+    import mp3_parse.mp3_parse
+if user_input == 'mp3':
+    mp3_parse()
+
+def gen_jpeg_argumnets():
+    import file_to_omni_argumnets_txt.jpeg_file_to_omni_argumnets_txt
+if user_input == 'iga':
+    gen_jpeg_argumnets()
+
+def gen_mp3_argumnets():
+    import file_to_omni_argumnets_txt.mp3_file_to_omni_argumnets_txt
+if user_input == 'mp3ga':
+    gen_mp3_argumnets()
 
 def omni_sendissuancefixed():
     import arguments_txt_to_rpc.omni_sendissuancefixed #be carful this one creates transactions.
@@ -33,3 +48,9 @@ def sendissuancefixed_singal():
     import sifs.sendissuancefixed_singal
 if user_input == 'sifs':
     sendissuancefixed_singal()
+
+
+def ipfs_milti_nfts():
+    import ipfs_multi_nfts.ipfs_milti_nfts_to_omni_argumnets_txt
+if user_input == 'ipfs':
+    ipfs_milti_nfts()
